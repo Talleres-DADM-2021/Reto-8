02114199e22f546e63c455163e1a3c139ecec229
Elsa Johanna Arias <ejariasm@unal.edu.co>
2022-01-13 01:44:45 -0500

    Reto 8

---
+++ app/src/main/java/com/example/reto8/EmpresaActivity.java
@@ -0,0 +1,34 @@
+package com.example.reto8;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import reto8.R;
+
+public class EmpresaActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerViewEmpresa;
+    private RecyclerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empresas);
+
+
+        recyclerViewEmpresa = (RecyclerView)findViewById(R.id.recyclerEmpresas);
+        recyclerViewEmpresa.setLayoutManager(new LinearLayoutManager(this));
+
+        DevelopBD developBD = new DevelopBD(getApplicationContext());
+
+        adapter = new RecyclerAdapter(developBD.showEmpresas());
+        recyclerViewEmpresa.setAdapter(adapter);
+    }
+
+}
